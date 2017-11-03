d8fc5f41e1a7df8a3b02ef1fb5815198d02d8883
Torsten <torsten.heinrich@posteo.net>
2017-11-03 02:42:21 +0100

    moved simulation parameters to isleconfig.py config file

--- start.py
+++ start.py
@@ -5,6 +5,7 @@ import sys, pdb
 import numba as nb
 import isleconfig
 
+simulation_parameters = isleconfig.simulation_parameters
 replic_ID = None
 override_no_riskmodels = False
 
@@ -48,32 +49,6 @@ from insurancesimulation import InsuranceSimulation
 from insurancefirm import InsuranceFirm
 from riskmodel import RiskModel
 from reinsurancefirm import ReinsuranceFirm
-
-
-simulation_parameters={"no_categories": 2,
-                       "no_insurancefirms": 20,
-                       "no_reinsurancefirms": 2,
-                       "no_riskmodels": 2,
-                       "norm_profit_markup": 0.15,
-                       "rein_norm_profit_markup": 0.15,
-                       "mean_contract_runtime": 30,
-                       "contract_runtime_halfspread": 10,
-                       "max_time": 60,
-                       "money_supply": 2000000000,
-                       "event_time_mean_separation": 100 / 3.,
-                       "expire_immediately": True,
-                       "risk_factors_present": False,
-                       "risk_factor_lower_bound": 0.4,
-                       "risk_factor_upper_bound": 0.6,
-                       "initial_acceptance_threshold": 0.5,
-                       "acceptance_threshold_friction": 0.9,
-                       "initial_agent_cash": 10000,
-                       "initial_reinagent_cash": 50000,
-                       "interest_rate": 0,
-                       "reinsurance_limit": 0.1,
-                       "upper_price_limit": 1.2,
-                       "lower_price_limit": 0.85,
-                       "no_risks": 20000}
 
 #@gui(simulation_parameters, serve=True)
 def main(simulation_parameters):
