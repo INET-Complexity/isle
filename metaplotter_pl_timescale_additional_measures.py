3e3d332224c237713a03a7fdd3de79d8b4a21721
Torsten <torsten.heinrich@posteo.net>
2018-07-15 17:27:45 +0200

    plotting of additional measures

--- metaplotter_pl_timescale_additional_measures.py
+++ metaplotter_pl_timescale_additional_measures.py
@@ -66,7 +66,7 @@ def read_data():
 def plotting(output_label, timeseries_dict, riskmodelsetting1, riskmodelsetting2, series1, series2=None, additionalriskmodelsetting3=None, additionalriskmodelsetting4=None, plottype1="mean", plottype2="mean"):
     # dictionaries
     colors = {"one": "red", "two": "blue", "three": "green", "four": "yellow"}
-    labels = {"profitslosses": "Profits and Losses (Insurer)", "contracts": "Contracts (Insurers)", "cash": "Liquidity (Insurers)", "operational": "Active Insurers", "premium": "Premium", "reinprofitslosses": "Profits and Losses (Reinsurer)", "reincash": "Liquidity (Reinsurers)", "reincontracts": "Contracts (Reinsurers)", "reinoperational": "Active Reinsurers"}
+    labels = {"reinexcess_capital": "Excess Capital (Reinsurers)", "excess_capital": "Excess Capital (Insurers)", "cumulative_unrecovered_claims": "Uncovered Claims (cumulative)", "cumulative_bankruptcies": "Bankruptcies (cumulative)", "profitslosses": "Profits and Losses (Insurer)", "contracts": "Contracts (Insurers)", "cash": "Liquidity (Insurers)", "operational": "Active Insurers", "premium": "Premium", "reinprofitslosses": "Profits and Losses (Reinsurer)", "reincash": "Liquidity (Reinsurers)", "reincontracts": "Contracts (Reinsurers)", "reinoperational": "Active Reinsurers"}
     
     # prepare labels, timeseries, etc.
     color1 = colors[riskmodelsetting1]
@@ -147,30 +147,41 @@ def plotting(output_label, timeseries_dict, riskmodelsetting1, riskmodelsetting2
 
 timeseries = read_data()
 
-## for just two different riskmodel settings
-#plotting(output_label="fig_pl_survival_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
-#    riskmodelsetting2="two", series1="profitslosses", series2="operational", plottype1="mean", plottype2="median")
-#plotting(output_label="fig_reinsurers_pl_survival_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
-#    riskmodelsetting2="two", series1="reinprofitslosses", series2="reinoperational", plottype1="mean", plottype2="median")
+# for just two different riskmodel settings
+#plotting(output_label="fig_pl_excap_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+#    riskmodelsetting2="two", series1="profitslosses", series2="excess_capital", plottype1="mean", plottype2="mean")
+#plotting(output_label="fig_reinsurers_pl_excap_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+#    riskmodelsetting2="two", series1="reinprofitslosses", series2="reinexcess_capital", plottype1="mean", plottype2="mean")
+#plotting(output_label="fig_bankruptcies_unrecovered_claims_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+#    riskmodelsetting2="two", series1="cumulative_bankruptcies", series2="cumulative_unrecovered_claims", plottype1="mean", plottype2="median")
 #plotting(output_label="fig_premium_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", riskmodelsetting2="two", \
 #    series1="premium", series2=None, plottype1="mean", plottype2=None)
 #
 #raise SystemExit
 # for four different riskmodel settings
-plotting(output_label="fig_pl_survival_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
-        riskmodelsetting2="two", series1="profitslosses", series2="operational", additionalriskmodelsetting3="three", \
+plotting(output_label="fig_pl_excap_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+    riskmodelsetting2="two", series1="profitslosses", series2="excess_capital", additionalriskmodelsetting3="three", \
+        additionalriskmodelsetting4="four", plottype1="mean", plottype2="mean")
+plotting(output_label="fig_reinsurers_pl_excap_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+    riskmodelsetting2="two", series1="reinprofitslosses", series2="reinexcess_capital", additionalriskmodelsetting3="three", \
+        additionalriskmodelsetting4="four", plottype1="mean", plottype2="mean")
+plotting(output_label="fig_bankruptcies_unrecovered_claims_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
+    riskmodelsetting2="two", series1="cumulative_bankruptcies", series2="cumulative_unrecovered_claims", additionalriskmodelsetting3="three", \
         additionalriskmodelsetting4="four", plottype1="mean", plottype2="median")
-plotting(output_label="fig_pl_survival_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", \
-        riskmodelsetting2="four", series1="profitslosses", series2="operational",  additionalriskmodelsetting3="one", \
-        additionalriskmodelsetting4="two", plottype1="mean", plottype2="median")
-plotting(output_label="fig_reinsurers_pl_survival_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", \
-        riskmodelsetting2="two", series1="reinprofitslosses", series2="reinoperational", \
-        additionalriskmodelsetting3="three", additionalriskmodelsetting4="four", plottype1="mean", plottype2="median")
-plotting(output_label="fig_reinsurers_pl_survival_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", \
-        riskmodelsetting2="four", series1="reinprofitslosses", series2="reinoperational", \
-        additionalriskmodelsetting3="one", additionalriskmodelsetting4="two", plottype1="mean", plottype2="median")
 plotting(output_label="fig_premium_1_2", timeseries_dict=timeseries, riskmodelsetting1="one", riskmodelsetting2="two", \
-        series1="premium", series2=None, additionalriskmodelsetting3="three", additionalriskmodelsetting4="four", \
-        plottype1="mean", plottype2=None)
+    series1="premium", series2=None, additionalriskmodelsetting3="three", additionalriskmodelsetting4="four", plottype1="mean", plottype2=None)
+
+plotting(output_label="fig_pl_excap_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", \
+    riskmodelsetting2="four", series1="profitslosses", series2="excess_capital", additionalriskmodelsetting3="one", \
+        additionalriskmodelsetting4="two", plottype1="mean", plottype2="mean")
+plotting(output_label="fig_reinsurers_pl_excap_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", \
+    riskmodelsetting2="four", series1="reinprofitslosses", series2="reinexcess_capital", additionalriskmodelsetting3="one", \
+        additionalriskmodelsetting4="two", plottype1="mean", plottype2="mean")
+plotting(output_label="fig_bankruptcies_unrecovered_claims_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", \
+    riskmodelsetting2="four", series1="cumulative_bankruptcies", series2="cumulative_unrecovered_claims", additionalriskmodelsetting3="one", \
+        additionalriskmodelsetting4="two", plottype1="mean", plottype2="median")
+plotting(output_label="fig_premium_3_4", timeseries_dict=timeseries, riskmodelsetting1="three", riskmodelsetting2="four", \
+    series1="premium", series2=None, additionalriskmodelsetting3="one", additionalriskmodelsetting4="two", plottype1="mean", plottype2=None)
+
 
 #pdb.set_trace()
