760000e6c1cbd5b5655635b283442f7cff497b92
Torsten <torsten.heinrich@posteo.net>
2018-11-22 19:49:07 +0000

    Added commenting in calibration scripts

--- calibrationscore.py
+++ calibrationscore.py
@@ -1,24 +1,51 @@
+"""CalibrationScore class. Defines calibration score object. Can compute and print the calibration score
+   based on functions imported from calibration_conditions.py by component and combined."""
+
 import logger
 from inspect import getmembers, isfunction
 import numpy as np
 
-import calibration_conditions
+import calibration_conditions   # Test functions
 
 class CalibrationScore():
     def __init__(self, L):
+        """Constructor method.
+            Arguments:
+                L: Type: Logger object. The log of a single simulation run.
+            Returns instance."""
+
+        """Assert sanity of log and save log."""
         assert isinstance(L, logger.Logger)
         self.logger = L
+        
+        """Prepare list of calibration tests from calibration_conditions.py"""
         self.conditions = [f for f in getmembers(calibration_conditions) if isfunction(f[1])]
+        
+        """Prepare calibration score variable."""
         self.calibration_score = None
         
     def test_all(self):
+        """Method to test all calibration tests.
+            No arguments.
+            Returns combined calibration score as float \in [0,1]."""
+        
+        """Compute score components"""
         scores = {condition[0]: condition[1](self.logger) for condition in self.conditions}
+        """Print components"""
         print("\n")
         for cond_name, score in scores.items():
             print("{0:47s}: {1:8f}".format(cond_name, score))
+        """Compute combined score"""
         self.calibration_score = self.combine_scores(np.array([*scores.values()], dtype=object))
+        """Print combined score"""
         print("\n                        Total calibration score: {0:8f}".format(self.calibration_score))
+        """Return"""
         return self.calibration_score
     
     def combine_scores(self, slist):
+        """Method to combine calibration score components. Combination is additive (mean). Change the function
+           for other combination methods (multiplicative or minimum).
+            Arguments:
+                slist: Type list of numeric or numpy array. List of component calibration scores.
+            Returns combined calibration score."""
         return np.nanmean(slist)
--- condition_aux.py
+++ condition_aux.py
@@ -1,4 +1,9 @@
+"""Auxiliary functions and data for calibration test functions"""
+
 import numpy as np
+
+
+"""Data"""
 
 """Bloomberg size data for US firms"""
 insurance_firm_sizes_empirical_2017 = [42.4701, 108.0418, 110.2641, 114.437, 130.2988, 133.674, 146.438, 152.3354, 
@@ -13,17 +18,43 @@ insurance_firm_sizes_empirical_2017 = [42.4701, 108.0418, 110.2641, 114.437, 130
 reinsurance_firm_sizes_empirical_2017 = [396.898, 627.808, 6644.189, 15226.131, 25384.317, 23591.792, 3357.393, 
                                          13606.422, 4671.794, 614.121, 60514.818, 24760.177, 2001.669, 182.2, 12906.4]
 
+"""Functions"""
+
 def condition_stationary_state(series):
+    """Stationarity test function for time series. Tests if the mean of the last 25% of the time series is within 1-2
+       standard deviation of the mean of the middle section (between 25% and 75% of the time series). The first
+       25% are not considered to discard the transient.
+        Arguments:
+            series: Type list of numeric or numpy array. The time series
+        Returns:
+            Calibration score between 0 and 1. Is 1 if last 25% are within one standard deviation, between 0 and 1 if
+                they are between 1 and 2 standard deviations, 0 otherwise."""
+                
+    """Compute means and standard deviation"""
     mean_reference = np.mean(series[int(len(series)*.25):int(len(series)*.75)])
     std_reference = np.std(series[int(len(series)*.25):int(len(series)*.75)])
     mean_test = np.mean(series[int(len(series)*.75):int(len(series)*1.)])
+    
+    """Compute score"""
     score = 1 + (np.abs(mean_test - mean_reference) - std_reference) / std_reference
     score = 1 if score>1 else score
     score = 0 if score<0 else score
+    
+    """Set score to one if standard deviation is zero"""
+    if score == np.nan and np.std(series[int(len(series)*.25):int(len(series)*.75)]) == 0:
+        score = 1
     return score
     
 
 def scaler(series): # TODO: find a better way to scale heavy-tailed distributions than to use standard score scaling on logs 
+    """Function to do a standard score scaling of the log of a heavy-tailed distribution. This is used to calibrate
+       distributions where the unit is not important (distributions of sizes of firms e.g.). This would be perfectly
+       appropriate for lognormal distributions, but should work reasonably well for calibration of other heavy-tailed 
+       distributions. An alternative would be a scaling robust towards outliers (as included in the sklearn package).
+        Arguments:
+            series: Type list of numeric or numpy array. The time series
+        Returns:
+            Calibratied series."""
     series = np.asarray(series)
     assert (series>1).all()
     logseries = np.log(series)
