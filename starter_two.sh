62edd6d60046280178d07ba3d2558b0590228dc9
rht <rhtbot@protonmail.com>
2018-12-05 04:58:32 +0000

    starter_*: Determine timestamp in advance to ensure they are identical

--- starter_two.sh
+++ starter_two.sh
@@ -1,12 +1,13 @@
-mv data/replication_rc_event_schedule.dat data/replication_rc_event_schedule.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/replication_randomseed.dat data/replication_randomseed.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_operational.dat data/two_operational.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_contracts.dat data/two_contracts.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_cash.dat data/two_cash.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_reinoperational.dat data/two_reinoperational.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_reincontracts.dat data/two_reincontracts.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_reincash.dat data/two_reincash.dat_$(date +%Y_%h_%d_%H_%M)
-mv data/two_premium.dat data/two_premium.dat_$(date +%Y_%h_%d_%H_%M)
+export timestamp="$(date +%Y_%h_%d_%H_%M)"  # to ensure they are identical
+mv data/replication_rc_event_schedule.dat{,_$timestamp}
+mv data/replication_randomseed.dat{,_$timestamp}
+mv data/two_operational.dat{,_$timestamp}
+mv data/two_contracts.dat{,_$timestamp}
+mv data/two_cash.dat{,_$timestamp}
+mv data/two_reinoperational.dat{,_$timestamp}
+mv data/two_reincontracts.dat{,_$timestamp}
+mv data/two_reincash.dat{,_$timestamp}
+mv data/two_premium.dat{,_$timestamp}
 
 for ((i=0; i<300; i++)) do
     python3 start.py --replicid $i 
