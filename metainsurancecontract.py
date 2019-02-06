8811f1feae61e4804a043a4a294c73d8442fd59a
Juan Sabuco <juan.sabuco@urjc.es>
2019-02-06 22:09:17 +0000

    Add roll over flag.

--- metainsurancecontract.py
+++ metainsurancecontract.py
@@ -76,6 +76,10 @@ class MetaInsuranceContract():
             self.contract.reinsure(reinsurer=self.insurer, reinsurance_share=properties["reinsurance_share"], \
                                    reincontract=self)
 
+        self.roll_over_flag = 0
+
+
+
     def check_payment_due(self, time):
         if len(self.payment_times) > 0 and time >= self.payment_times[0]:
             # Create obligation for premium payment
